c55f4dcaf1ab5d1f49c539f9003f94c6f52eec5b
junyoung4213 <junyoung4213@gmail.com>
2021-07-24 01:09:22 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaft/ThymeleaftBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymeleaft/ThymeleaftBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymeleaft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleaftBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
